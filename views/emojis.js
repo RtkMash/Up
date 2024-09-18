fdd843c25dc3814b60904aa9155438c6cf677976
Rtk Mash <166203168+RtkMash@users.noreply.github.com>
2024-09-18 13:16:58 +0300

    Add files via upload

---
+++ views/emojis.js
@@ -0,0 +1,69 @@
+// @ts-check
+
+const Tik = "<a:tick:1266087134733799425>";
+const Çarpı = "<a:R_Basarisiz:1246778414124371988>";
+const Link = "<:zncr:1266091060531167315> ";
+const Yasak = "<a:R_Dikkat:1250516683097903226>";
+const Bakım = "<a:R_Doymak:1252248940393271338>";
+const Kullanıcı = "";
+const İsim = "";
+const Id = "";
+const Belge = "<a:R_Not:1246773351930794058>";
+const Premium = "<a:prem:1266105158132039773> ";
+const Yazı = "<:Yazi:1048677751818821702>";
+const Bot = "<a:R_Ayar:1246778410714402868>";
+const Duyuru = "<a:R_Duyuru:1246773344448024576>";
+const Davet = "<:zncr:1266091060531167315> ";
+const Çöp = "<:Cop:1066000658190311424>";
+const Saat = "<:Saat:1065251905497993347>";
+const Ping = "";
+const Limit = "<a:Ak:1250521169212342284>";
+const Karaliste = "<:Karaliste:1047167116727550023>";
+const Yetkili = "<:Yetkili:1047167457703497728>";
+const Sebep = "<:Sebep:1047168561392660602>";
+const Tac = "";
+const Js = "";
+const Node = "";
+const Uptime = "";
+const Sunucu = "";
+const Ram = "";
+const Giriş = "<:Giris:1048294668707835984>";
+const Çıkış = "<:Cikis:1048294654141010010>";
+const Hata = "<:Hata:1048294942054809630>";
+const Destek = "<a:destek:1266092480542150808>";
+const Oy = "<:Oy:1067128355830366228>";
+
+module.exports = {
+  Tik,
+  Çarpı,
+  Link,
+  Yasak,
+  Bakım,
+  Kullanıcı,
+  İsim,
+  Id,
+  Belge,
+  Premium,
+  Yazı,
+  Bot,
+  Duyuru,
+  Davet,
+  Çöp,
+  Saat,
+  Ping,
+  Limit,
+  Karaliste,
+  Yetkili,
+  Sebep,
+  Tac,
+  Js,
+  Node,
+  Uptime,
+  Sunucu,
+  Ram,
+  Giriş,
+  Çıkış,
+  Hata,
+  Destek,
+  Oy,
+};
